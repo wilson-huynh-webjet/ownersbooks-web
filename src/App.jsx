a90375be216a43a4d17c09cf0e78106555826f1c
wilson-huynh-webjet <wilson.huynh@webjet.com.au>
2026-06-26 12:09:48 +1000

    add filter panel with button and link

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import BookList from "./components/BookList";
 
 export default function App() {
   return (
-    <div id="book-list-panel">
-      <BookList />
-      <BookList />
-    </div>
+    <>
+      <div id="book-list-panel">
+        <BookList />
+        <BookList />
+      </div>
+      <div id="filter-panel">
+        <a href="#">Hardcover only</a>
+        <button>Get Books</button>
+      </div>
+    </>
   );
 }
